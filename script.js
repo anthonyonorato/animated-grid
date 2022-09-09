5fd56e6fe5d09ed472c1c4811c6c3eb0ec9570a9
Anthony Onorato <106277525+anthonyonorato@users.noreply.github.com>
2022-09-08 23:03:54 -0400

    create grid with js

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ class App extends React.Component {
       rows: 0,
       total: 1
     };
+     
+      getGridSize = () => {
+        const columns = Math.floor(document.body.clientWidth / 50);
+        const rows = Math.floor(document.body.clientHeight / 50);
+    
+        this.setState({ columns, rows, total: rows * columns });
+        anime({
+          targets: ".grid-item",
+          backgroundColor: "#fff",
+          duration: 0,
+          easing: "linear"
+        });
+      };
 
-  
+    
